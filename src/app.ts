517c128475d9060621a5af417f7186ae5b628a35
Zovalger <74253343+Zovalger@users.noreply.github.com>
2024-01-13 11:59:19 -0400

    fix public folder dir

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,12 @@ if (NODE_ENV !== "production") app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"../", "public")));
+
+if (NODE_ENV !== "production") {
+	app.use(express.static(path.join(__dirname, "../", "public")));
+} else {
+	app.use(express.static(path.join(__dirname, "../", "../", "public")));
+}
 
 app.use(
 	session({
